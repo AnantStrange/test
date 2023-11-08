c00a64ca5985e0f55f745c585f57fed413a07266
Iamneo <techadmin@iamneo.ai>
2023-11-08 14:17:39 +0000

    Backup Commit

--- angularapp/src/app/app.module.ts
+++ angularapp/src/app/app.module.ts
@@ -3,11 +3,19 @@ import { NgModule } from '@angular/core';
 import {Navbar} from './Navbar/navbar.component';
 import { AppRoutingModule } from './routing/app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './Navbar/navbar.component';
+import { StudentListComponent } from './student-list/student-list.component';
+import { StudentAddComponent } from './student-add/student-add.component';
+import { StudentDetailsComponent } from './student-details/student-details.component';
 
 @NgModule({
   declarations: [
     AppComponent[Symbol],
-    Navbar
+    Navbar,
+    NavbarComponent,
+    StudentListComponent,
+    StudentAddComponent,
+    StudentDetailsComponent
   ],
   imports: [
     BrowserModule,
